26598241d18aeeb5c22f007991920b34f921d544
inafcode <naffiilham@gmail.com>
2026-05-19 17:09:03 +0700

    #

---
+++ studyCase/cek-login.ts
@@ -0,0 +1,35 @@
+function cekLogin(username: string, password: string) {
+    if (username == "admin" && password == "12345") {
+        return "Login Berhasil";
+    } else if (username == "user" && password == "12345") {
+        return "Login Berhasil";
+    } else {
+        return "Login Gagal";
+    }
+}
+
+function cekAkses(role: string, statusLogin: string) {
+    if (statusLogin == "Login Berhasil") {
+        if (role == "admin") {
+            return "Berhasil Akses Dashboard Admin";
+        } else if (role == "user") {
+            return "Berhasil Akses Dashboard User";
+        } else {
+            return "Role Tidak Terdaftar";
+        }
+    } else {
+        return "Akses Gagal";
+    }
+}
+
+const username = "admin";
+const password = "12345";
+const role = "admin";
+
+const statusLogin = cekLogin(username, password);
+const akses = cekAkses(role, statusLogin);
+
+console.log(`Username: ${username}`);
+console.log(`Role: ${role}`);
+console.log(`Akses: ${akses}`);
+console.log(`Status Login: ${statusLogin}`);
