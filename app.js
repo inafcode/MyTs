69f8a03ff16a87c2164ec5f906fb3170c7899355
inafcode <naffiilham@gmail.com>
2026-05-19 11:08:11 +0700

    Day 1

---
+++ app.js
@@ -0,0 +1,55 @@
+"use strict";
+console.log("Belajar hari ke 1");
+let nama = "Naufal";
+let umur = 20;
+console.log(`Nama teman saya : ${nama}`);
+console.log(`Umur teman saya : ${umur}`);
+if (umur >= 20) {
+    console.log(`${nama} sudah dewasa`);
+}
+else {
+    console.log(`${nama} masih kecil`);
+}
+console.log("=======================================");
+const nama1 = "Ilham";
+let isSudahMenikah = false;
+let umur1 = 22;
+console.log(`Nama saya : ${nama1}`);
+console.log(`Umur saya : ${umur1}`);
+console.log(`Status Menikah : ${isSudahMenikah}`);
+if (umur1 >= 25) {
+    console.log(`${umur1} sudah boleh menikah`);
+}
+else {
+    console.log(`${umur1} belum boleh menikah`);
+}
+let tahunDepan = umur1 + 1;
+console.log(`Tahun depan umur saya : ${tahunDepan}`);
+console.log("=======================================");
+const name2 = "Naffi";
+let usia = 24;
+let hobi = "Coding";
+let isSudahMakan = true;
+let usiaTahunDepan = usia + 1;
+console.log(`Nama saya : ${name2}`);
+console.log(`Usia saya : ${usia}`);
+console.log(`Hobi saya : ${hobi}`);
+console.log(`Apa Sudah Makan? ${isSudahMakan}`);
+console.log(`Usia Saya Tahun Depan : ${usiaTahunDepan}`);
+console.log("=======================================");
+let umurUser = 15;
+let punyaKTP = false;
+console.log(`Umur User : ${umurUser}`);
+if (punyaKTP) {
+    console.log("Punya KTP");
+}
+else {
+    console.log("Belum Punya KTP");
+}
+if (umurUser >= 17 && punyaKTP) {
+    console.log(`${umurUser} sudah boleh membuat SIM`);
+}
+else {
+    console.log(`${umurUser} belum boleh membuat SIM`);
+}
+console.log(`===========================================`);
---
+++ app.ts
@@ -0,0 +1,65 @@
+console.log("Belajar hari ke 1")
+
+let nama: string = "Naufal";
+let umur: number = 20;
+
+console.log(`Nama teman saya : ${nama}`);
+console.log(`Umur teman saya : ${umur}`);
+if (umur >= 20) {
+    console.log(`${nama} sudah dewasa`)
+} else {
+    console.log(`${nama} masih kecil`)
+}
+
+console.log("=======================================");
+
+const nama1: string = "Ilham";
+let isSudahMenikah: boolean = false;
+let umur1: number = 22;
+
+console.log(`Nama saya : ${nama1}`);
+console.log(`Umur saya : ${umur1}`);
+console.log(`Status Menikah : ${isSudahMenikah}`);
+if (umur1 >= 25) {
+    console.log(`${umur1} sudah boleh menikah`)
+} else {
+    console.log(`${umur1} belum boleh menikah`)
+}
+
+let tahunDepan: number = umur1 + 1;
+
+console.log(`Tahun depan umur saya : ${tahunDepan}`);
+console.log("=======================================");
+
+
+const name2: string = "Naffi";
+let usia: number = 24;
+let hobi: string = "Coding";
+let isSudahMakan: boolean = true;
+let usiaTahunDepan: number = usia + 1;
+
+console.log(`Nama saya : ${name2}`);
+console.log(`Usia saya : ${usia}`);
+console.log(`Hobi saya : ${hobi}`);
+console.log(`Apa Sudah Makan? ${isSudahMakan}`);
+console.log(`Usia Saya Tahun Depan : ${usiaTahunDepan}`);
+console.log("=======================================");
+
+
+let umurUser: number = 15;
+let punyaKTP: boolean = false;
+
+console.log(`Umur User : ${umurUser}`);
+if (punyaKTP) {
+    console.log("Punya KTP");
+} else {
+    console.log("Belum Punya KTP");
+}
+
+if (umurUser >= 17 && punyaKTP) {
+    console.log(`${umurUser} sudah boleh membuat SIM`);
+} else {
+    console.log(`${umurUser} belum boleh membuat SIM`);
+}
+
+console.log(`===========================================`);
