19f6be0014bae51d96383ee694deb96cb8591f2f
inafcode <naffiilham@gmail.com>
2026-05-23 16:36:12 +0700

    #

--- mini-projects/aset.js
+++ mini-projects/aset.js
@@ -21,17 +21,29 @@ class TemplateBaju extends AsetVirtual {
         super(namaInput, hargaInput);
         this.tema = temaInput;
     }
+    terapkanDiskon(persen) {
+        this.harga = this.harga - (this.harga * (persen / 100));
+    }
     // Method khusus untuk class anak
     cekDetailBaju() {
         // Kita bisa memanggil this.namaKarya karena sekarang statusnya "protected"
         return `Baju virtual ${this.namaKarya} ini memiliki tema ${this.tema}.`;
     }
+    cekInfo() {
+        return `${this.namaKarya} (Tema: ${this.tema}) dijual seharga Rp.${this.harga}. Terjual: ${this.totalTerjual} kali.`;
+    }
 }
-const karyaPertama = new AsetVirtual("Peta Gunung", 150);
+class Peta3D extends AsetVirtual {
+    constructor(namaInput, hargaInput) {
+        super(namaInput, hargaInput);
+    }
+}
+const karyaPertama = new Peta3D("Peta 3D", 150);
 karyaPertama.catatPenjualan(3);
 karyaPertama.catatPenjualan(2);
 console.log(karyaPertama.cekInfo());
 const bajuBaru = new TemplateBaju("Kemeja Kasual", 500, "Dark Elegant");
+bajuBaru.terapkanDiskon(50);
 bajuBaru.catatPenjualan(8);
 console.log(bajuBaru.cekInfo());
 console.log(bajuBaru.cekDetailBaju());
--- mini-projects/aset.ts
+++ mini-projects/aset.ts
@@ -1,4 +1,8 @@
-class AsetVirtual {
+interface BisaDiskon {
+    terapkanDiskon(persen: number): void;
+}
+
+abstract class AsetVirtual {
     protected namaKarya: string;
     protected harga: number;
     protected totalTerjual: number;
@@ -19,13 +23,16 @@ class AsetVirtual {
 }
 
 
-class TemplateBaju extends AsetVirtual {
+class TemplateBaju extends AsetVirtual implements BisaDiskon{
     private tema: string;
 
     constructor(namaInput: string, hargaInput: number, temaInput: string) {
         super(namaInput, hargaInput); 
-        
         this.tema = temaInput;
+    }
+
+    terapkanDiskon(persen: number): void {
+        this.harga = this.harga - (this.harga * (persen / 100));
     }
 
     // Method khusus untuk class anak
@@ -33,10 +40,20 @@ class TemplateBaju extends AsetVirtual {
         // Kita bisa memanggil this.namaKarya karena sekarang statusnya "protected"
         return `Baju virtual ${this.namaKarya} ini memiliki tema ${this.tema}.`;
     }
+
+    cekInfo() {
+        return `${this.namaKarya} (Tema: ${this.tema}) dijual seharga Rp.${this.harga}. Terjual: ${this.totalTerjual} kali.`;
+    }
+}
+
+class Peta3D extends AsetVirtual {
+    constructor(namaInput: string, hargaInput: number) {
+        super(namaInput, hargaInput);
+    }
 }
 
 
-const karyaPertama = new AsetVirtual("Peta Gunung", 150);
+const karyaPertama = new Peta3D("Peta 3D", 150);
 
 karyaPertama.catatPenjualan(3);
 karyaPertama.catatPenjualan(2);
@@ -45,6 +62,7 @@ console.log(karyaPertama.cekInfo());
 
 const bajuBaru = new TemplateBaju("Kemeja Kasual", 500, "Dark Elegant");
 
+bajuBaru.terapkanDiskon(10);
 bajuBaru.catatPenjualan(8);
 console.log(bajuBaru.cekInfo());
 console.log(bajuBaru.cekDetailBaju());
